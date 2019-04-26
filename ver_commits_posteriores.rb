3cabbde2e566daa07c77ab4d7f1f5c48838a5973
Federico Aloi <federico.aloi@gmail.com>
2019-04-26 17:08:01 -0300

    No se imprimen líneas de más en ver commits posteriores

--- ver_commits_posteriores.rb
+++ ver_commits_posteriores.rb
@@ -7,5 +7,6 @@ raise "Necesito como parámetro la fecha y hora (ejemplo: 2019-04-24 23:59:59)."
 $fecha = ARGV[0]
 
 foreach_repo(false) do
-  puts `git --no-pager log --date=local --after="#{$fecha}" --pretty`
+  resultado = `git --no-pager log --date=local --after="#{$fecha}" --pretty`
+  puts resultado if not resultado.strip.empty?
 end
