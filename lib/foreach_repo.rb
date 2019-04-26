200b4740664ffef81a2bcd905c3e12e8e94b027e
Federico Aloi <federico.aloi@gmail.com>
2019-04-26 17:20:50 -0300

    foreach_repo ordena las carpetas

--- lib/foreach_repo.rb
+++ lib/foreach_repo.rb
@@ -1,7 +1,7 @@
 $working_dir = Dir.pwd
 
 def foreach_repo(log_enabled = true)
-  repos = Dir.glob('*').select {|f| File.directory? f}
+  repos = Dir.glob('*').select {|f| File.directory? f}.sort
 
   for repo in repos
     print_log log_enabled, "Trabajando con #{repo}..."
