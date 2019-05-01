8732fd291894bc42feb1689c3b5eb36e0618d5f2
Federico Aloi <federico.aloi@gmail.com>
2019-04-30 21:08:17 -0300

    Comando actualizar

--- lib/yanapiri.rb
+++ lib/yanapiri.rb
@@ -40,6 +40,22 @@ module Yanapiri
       $bot.clonar_entrega!(nombre)
     end
 
+    option :repo_base, {required: true, aliases: :b}
+    desc 'actualizar [ENTREGA]', 'Actualiza cada repositorio con el contenido que haya en el repositorio base'
+    def actualizar(nombre)
+      path_repo_base = "#{nombre}-base"
+      `git clone git@github.com:#{options.repo_base}.git #{path_repo_base}`
+
+      foreach_repo(nombre) do
+        `git remote rm base`
+        `git remote add base ../../#{path_repo_base}`
+        `git pull base master`
+        `git push origin master`
+      end
+
+      `rm -rf #{path_repo_base}`
+    end
+
     desc 'corregir [ENTREGA]', 'Prepara la entrega para la corrección, creando los archivos y el pull request'
     option :commit_base, {required: true, aliases: :b}
     option :fecha_limite, {default: Time.now.to_s, aliases: :l}
