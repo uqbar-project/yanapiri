f5ae98b10463fe7841d851d4092b13eae49d6c62
Federico Aloi <federico.aloi@gmail.com>
2019-05-02 13:08:59 -0300

    No fallan los tests

--- lib/yanapiri.rb
+++ lib/yanapiri.rb
@@ -87,7 +87,7 @@ module Yanapiri
 end
 
 organization = 'obj1-unahur-2019s1'
-gh_token = ENV['YANAPIRI_GH_TOKEN'] or raise "Token de GitHub no encontrado, asegurate de que está guardado en la variable de entorno YANAPIRI_GH_TOKEN. Si no tenés un token, podés generarlo en https://github.com/settings/tokens, con al menos scope 'repo'."
+gh_token = ENV['YANAPIRI_GH_TOKEN']
 
 $bot = Bot.new(organization, gh_token)
 
